3f21e52e23656267340b8cc0f12ffe2559e09281
Aadil Mamujee <amamujee@gmail.com>
2026-05-08 13:28:33 -0400

    Add GitHub link to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <div className="absolute inset-x-0 bottom-0 h-16 rounded-t-[50%] bg-[#4a271b]" />
 
         <div className="relative z-10 flex w-full flex-col px-5 py-5 sm:px-8 lg:px-12">
-          <header className="flex items-center">
+          <header className="flex items-center justify-between gap-4">
             <Link href="/" className="flex items-center gap-3" aria-label="Burrow home">
               <Image
                 src="/icons/burrow-icon-64.png"
@@ -35,6 +35,14 @@ export default function Home() {
               />
               <span className="text-xl font-black tracking-normal">Burrow</span>
             </Link>
+            <a
+              href="https://github.com/amamujee/burrow"
+              target="_blank"
+              rel="noreferrer"
+              className="rounded-lg border-2 border-[#082329] bg-[#fffaf4]/85 px-3 py-2 text-sm font-black text-[#102f36] shadow-[2px_2px_0_#082329] transition hover:-translate-y-0.5 hover:bg-white"
+            >
+              GitHub
+            </a>
           </header>
 
           <div className="grid flex-1 items-center gap-10 py-12 lg:grid-cols-[minmax(0,0.9fr)_minmax(460px,0.9fr)] lg:py-8">
